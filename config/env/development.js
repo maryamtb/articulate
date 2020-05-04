064abebc7067e5f8c9cc234c0770777731f776e6
maryamtb <maryamtb.17@gmail.com>
2020-05-04 23:09:48 +0000

    gitignore update

--- config/env/development.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-db: function(callback) {
-var MongoClient = require('mongodb').MongoClient
-    MongoClient.connect('mongodb+srv://a:<a>@cluster1-pyaia.mongodb.net/test?retryWrites=true&w=majority', callback)
-  }
-};
